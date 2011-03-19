1230257562bfa15b1c223b5be2d7a69dee707bde
tridung0711@gmail.com <tridung0711@gmail.com@098a8621-e33c-f5ee-a0c2-1333193236c2>
2011-03-19 04:16:17 +0000

    First Shopping Cart PROJECTS Version 0.0

---
+++ my-project-shoppingcard/src/com/uet/shoppingcart/ShoppingCart.java
@@ -0,0 +1,13 @@
+package com.uet.shoppingcart;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoppingCart extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
